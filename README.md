f168164909b0b4412af98678e9b3bb892840af34
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2022-11-19 16:38:32 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# React Trello Clone
+
+A small Trello clone built with React Material UI and Redux.
+## Usage
+
+In the project directory, you can run:
+
+### `yarn start`
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import TrelloBoard from './containers/TrelloBoard';
+
+const App = () => {
+  return (
+    <TrelloBoard/>
+  );
+};
+
+export default App;
